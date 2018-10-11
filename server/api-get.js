b76f69a52e32d5b78b66ff3adedcb36b33b567aa
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-10-11 17:17:02 -0400

    feat: server api-get

---
+++ server/api-get.js
@@ -0,0 +1,29 @@
+// @flow
+const got = require('got')
+
+/**
+ * A simple wrapper around `got` that fetches the resource using the token for auth, if available.
+ *
+ * At some point this needs to use configurable Keyv cache stores.
+ */
+async function get (url /*: string */, token /*: string */) {
+  let headers
+  if (token) {
+    headers = {
+      Authorization: 'Basic ' + token
+    }
+  }
+  const options = {
+    headers,
+    json: true,
+    timeout: 1000
+  }
+  try {
+    const response = await got(url, options)
+    return response.body
+  } catch (error) {
+    return null
+  }
+}
+
+module.exports.get = get
