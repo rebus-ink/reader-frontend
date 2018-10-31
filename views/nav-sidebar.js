d19344b0ca01b019ebea9d7436616b6096be3eb1
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-10-31 17:18:33 -0400

    fix: nav sidebar accessibility

--- views/nav-sidebar.js
+++ views/nav-sidebar.js
@@ -2,6 +2,15 @@ export const navSidebarView = (render, model, req) => {
   const isSelected = path => {
     if (path === req.path) {
       return 'NavSidebar-item is-selected'
+    } else {
+      return 'NavSidebar-item'
+    }
+  }
+  const ariaCurrent = path => {
+    if (path === req.path) {
+      return 'page'
+    } else {
+      return false
     }
   }
   return render(model, ':navSidebarView')`<nav class="NavSidebar">
@@ -9,16 +18,24 @@ export const navSidebarView = (render, model, req) => {
   <ol>
     <li class="${isSelected(
     '/library'
-  )}"><a href="/library" class="NavSidebar-link">Library</a></li>
+  )}"><a href="/library" class="NavSidebar-link" aria-current=${ariaCurrent(
+  '/library'
+)}>Library</a></li>
     <li class="${isSelected(
     '/library/notes'
-  )}"><a href="/library/notes" class="NavSidebar-link">Notes</a></li>
+  )}"><a href="/library/notes" class="NavSidebar-link" aria-current=${ariaCurrent(
+  '/library/notes'
+)}>Notes</a></li>
     <li class="${isSelected(
     '/library/import'
-  )}"><a href="/library/import" class="NavSidebar-link">Import</a></li>
+  )}"><a href="/library/import" class="NavSidebar-link" aria-current=${ariaCurrent(
+  '/library/import'
+)}>Import</a></li>
     <li class="${isSelected(
     '/library/settings'
-  )}"><a href="/library/settings" class="NavSidebar-link">Settings</a></li>
+  )}"><a href="/library/settings" class="NavSidebar-link" aria-current=${ariaCurrent(
+  '/library/settings'
+)}>Settings</a></li>
   </ol>
 </nav>`
 }
