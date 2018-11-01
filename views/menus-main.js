3ac289d4475ba7f6bb98d0d50365ee857652009f
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-11-01 15:56:44 -0400

    fix: Menu adjustments

--- views/menus-main.js
+++ views/menus-main.js
@@ -3,9 +3,9 @@ export const topMenuMain = (render, model) => render(
   ':topMenuMain'
 )`<nav class="Menu Menu--top Menu--center">
 <ul class="Menu-list">
-  <li id="button-show-navigation-menu"><button class="TextButton" aria-label="Show Navigation menu">&lt; Menu</button></li>
-  <li><button class="TextButton">Filter…</button></li>
-  <li><button class="TextButton" aria-label="Information">Sort…</button></li>
+  <li id="button-show-navigation-menu"><button class="TextButton" aria-label="Show Navigation menu">Menu</button></li>
+  <li><button class="TextButton">Filter</button></li>
+  <li><button class="TextButton" aria-label="Information">Sort</button></li>
 </ul>
 </nav>`
 export const bottomMenuMain = (render, model) => render(
