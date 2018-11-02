c661259e9391a882f78d60078c4d39d29a68f69d
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-11-02 17:06:31 -0400

    fix: remove hide/show menu from infocard
    We'll do it another way.

--- views/menus-return.js
+++ views/menus-return.js
@@ -7,7 +7,6 @@ export const returnMenu = (render, model, req) => {
   }
   return render(model, ':returnMenu')`<nav class="Menu Menu--return">
 <ul class="Menu-list">
-  <li id="button-show-navigation-menu"><button class="TextButton" is="nav-menu-toggle" id="nav-menu-toggle" data-menu-id='NavSidebar'>Menu</button></li>
   <li id="button-return-library"><a href="${returnURL}" class="TextButton" aria-label="Return to Library">&lt; Return</a></li>
 </ul>
 </nav>`
