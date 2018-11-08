fc6949b0fdf213fc509525b742437c1f2ef6ae69
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-11-08 16:36:51 -0500

    fix: make getChapterState usable for book route

--- server/utils/get-chapter-state.js
+++ server/utils/get-chapter-state.js
@@ -5,7 +5,8 @@ const { arrify } = require('./arrify.js')
 async function getChapterState (req, res) {
   const book = await getBookState(req, res)
   const token = req.user.token
-  const chapterListing = arrify(book.orderedItems)[req.params.chapter]
+  const { chapterNumber = 0 } = req.params.chapter
+  const chapterListing = arrify(book.orderedItems)[chapterNumber]
   const id = chapterListing.id
   const result = await get(id, token)
   if (result === null) {
