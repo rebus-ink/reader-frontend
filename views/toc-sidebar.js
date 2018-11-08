e0dd57b7c8cf3d9375264ffd3e90366b23fdbd23
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-11-08 16:56:59 -0500

    feat: first pass at chapter template stubs

---
+++ views/toc-sidebar.js
@@ -0,0 +1,10 @@
+import { clean } from '../server/utils/sanitize-state'
+// Need to make sure this has a return link and location markers
+export const tocSidebarView = (render, model, req) => {
+  return render(
+    model,
+    ':tocSidebarView'
+  )`<div class="NavSidebar" id="NavSidebar">
+  ${[clean(model.book.toc.content)]}
+</div>`
+}
