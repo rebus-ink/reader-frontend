f3e08866efbecc651389b6b990ddc6e9fb3d4a47
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-11-21 16:59:42 -0500

    fix: chapter body

--- views/chapter.js
+++ views/chapter.js
@@ -2,7 +2,7 @@ import { clean } from '../server/utils/sanitize-state'
 
 export const chapterView = (render, model) => {
   const { chapter = {} } = model
-  return render(model, ':chapter')`<div class="Chapter">
+  return render(model, ':chapter')`<div class="Chapter-body">
 ${[clean(chapter.content)]}
 </div>`
 }
