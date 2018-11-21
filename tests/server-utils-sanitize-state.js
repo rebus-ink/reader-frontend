02658b8ba1ff50e83a27207855059fc7b6523562
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-11-21 16:30:42 -0500

    fix: sanitize downgrades heading levels by one

--- tests/server-utils-sanitize-state.js
+++ tests/server-utils-sanitize-state.js
@@ -5,10 +5,9 @@ tap.test('clean', test => {
   const dirtyHTML = `<h3>&quot;What if I want to allow only specific values on some attributes?&quot;</h3><p>When configuring the attribute in <code>allowedAttributes</code> simply use an object with attribute <code>name</code> and an allowed <code>values</code> array. In the following example <code>sandbox=&quot;allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-scripts&quot;</code> would become <code>sandbox=&quot;allow-popups allow-scripts&quot;</code>:</p>`
   test.equals(
     clean(
-      `<html><head></head><body>${dirtyHTML}<script></script></body></html>` +
-        '<script></script>'
+      `<html><head></head><body>${dirtyHTML}<script></script></body></html>`
     ),
-    dirtyHTML
+    dirtyHTML.replace(/h3/g, 'h4')
   )
   test.end()
 })
@@ -20,7 +19,7 @@ tap.test('makeModelSafe', test => {
     content: dirtyHTML + '<script></script>'
   }
   const safe = makeModelSafe(model)
-  test.equals(safe.toc, dirtyHTML)
-  test.equals(safe.content, dirtyHTML)
+  test.equals(safe.toc, dirtyHTML.replace(/h3/g, 'h4'))
+  test.equals(safe.content, dirtyHTML.replace(/h3/g, 'h4'))
   test.end()
 })
