15501ed9a7de7f0b8b05908cbfb60a04ec50b821
Baldur Bjarnason <bb@baldurbjarnason.com>
2018-11-21 15:47:18 -0500

    test: make sure that `clean` extracts the body tag

--- tests/server-utils-sanitize-state.js
+++ tests/server-utils-sanitize-state.js
@@ -3,7 +3,13 @@ const { clean, makeModelSafe } = require('../server/utils/sanitize-state.js')
 
 tap.test('clean', test => {
   const dirtyHTML = `<h3>&quot;What if I want to allow only specific values on some attributes?&quot;</h3><p>When configuring the attribute in <code>allowedAttributes</code> simply use an object with attribute <code>name</code> and an allowed <code>values</code> array. In the following example <code>sandbox=&quot;allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-scripts&quot;</code> would become <code>sandbox=&quot;allow-popups allow-scripts&quot;</code>:</p>`
-  test.equals(clean(dirtyHTML + '<script></script>'), dirtyHTML)
+  test.equals(
+    clean(
+      `<html><head></head><body>${dirtyHTML}<script></script></body></html>` +
+        '<script></script>'
+    ),
+    dirtyHTML
+  )
   test.end()
 })
 
