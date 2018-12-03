7722ed9fca15957466c3258f914517389904aad4
Baldur Bjarnason <baldur@rebus.foundation>
2018-12-03 17:54:06 +0000

    fix: media redirects for chapter route.

--- server/routes/reader-chapter.js
+++ server/routes/reader-chapter.js
@@ -7,6 +7,7 @@ const { getUserStreams } = require('../utils/get-user-streams.js')
 const express = require('express')
 const router = express.Router()
 const { getBookState } = require('../utils/get-book-state.js')
+const { arrify } = require('../utils/arrify.js')
 
 router.get('/reader/:bookId/:filePath', ensureLogin, getUserStreams, function (
   req,
@@ -15,6 +16,9 @@ router.get('/reader/:bookId/:filePath', ensureLogin, getUserStreams, function (
 ) {
   return getBookState(req, res)
     .then(model => {
+      if (model.chapter.type !== 'Document') {
+        res.redirect(getAlternate(model.chapter))
+      }
       const render = viperHTML.wire
       res.send(
         pageHead(render, model) +
@@ -24,5 +28,10 @@ router.get('/reader/:bookId/:filePath', ensureLogin, getUserStreams, function (
     })
     .catch(err => next(err))
 })
+
+function getAlternate (chapter) {
+  const urls = arrify(chapter.url)
+  return urls.filter(item => item.rel === 'alternate')[0].href
+}
 
 module.exports = router
