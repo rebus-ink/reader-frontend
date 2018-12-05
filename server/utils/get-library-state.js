18af541d09f5a80f3005ea00e3556004fbdcf862
Baldur Bjarnason <baldur@rebus.foundation>
2018-12-05 22:13:54 +0000

    fix: library state was always empty fix

--- server/utils/get-library-state.js
+++ server/utils/get-library-state.js
@@ -2,6 +2,7 @@ const { get } = require('./api-get.js')
 const { toBookCard } = require('../state/toBookCard.js')
 const { toBookCardAttribution } = require('../state/toBookCardAttribution.js')
 const { arrify } = require('./arrify.js')
+const debug = require('debug')('vonnegut:utils:get-library-state')
 
 async function getLibraryState (req, res) {
   const token = req.user.token
@@ -11,12 +12,14 @@ async function getLibraryState (req, res) {
     return result
   }
   const books = result.items.map(publication => {
+    debug(publication)
     const bookCard = toBookCard(publication)
     bookCard.attributions = arrify(publication.attributedTo).map(
       attribution => {
         return toBookCardAttribution(publication, attribution)
       }
     )
+    return bookCard
   })
   return { books }
 }
