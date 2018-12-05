6b09c58e78804aaf69f8aff1b717c4c9e142780c
Baldur Bjarnason <baldur@rebus.foundation>
2018-12-05 22:44:44 +0000

    fix: update info card to use correct model

--- views/info-card.js
+++ views/info-card.js
@@ -3,26 +3,27 @@ import { clean } from '../server/utils/sanitize-state'
 import { getId } from './utils/get-id.js'
 
 export const infoCardView = (render, model) => {
-  const { cover = {}, id = '' } = model
+  const { cover = {}, id = '' } = model.book
+  const book = model.book
   const url = `/reader/${encodeURIComponent(getId(id))}/0`
-  return render(model, ':infoCard')`<div class="InfoCard">
+  return render(book, ':infoCard')`<div class="InfoCard">
 <div class="InfoCard-title">
   <img  class="BookCard-icon" alt="${cover.summary}" src=${cover.url} width=${
   cover.width
 } height=${cover.height}> 
-  <h3 class="Row-title Row-title--cardTitle">${model.name}</h3>
+  <h3 class="Row-title Row-title--cardTitle">${book.name}</h3>
   <a class="Button Button--primary" href="${url}">Read</a>
 </div>
 <details class="InfoCard-detail" open>
   <summary>Metadata</summary>
   <ul class="InfoCard-contents">
-  ${attributionsMap(model.attributions, render)}
+  ${attributionsMap(book.attributions, render)}
   </ul>
 </details>
 <details class="InfoCard-detail">
   <summary>Table of Contents</summary>
   <div class="InfoCard-contents">
-    ${[clean(model.toc.content)]}
+    ${[clean(book.toc.content)]}
   </div>
 </details>
 <details class="InfoCard-detail">
