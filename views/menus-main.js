117a03ece848091ae4ad117e68b90839a270de86
Baldur Bjarnason <baldur@rebus.foundation>
2019-01-11 21:13:45 +0000

    style: menu fix

--- views/menus-main.js
+++ views/menus-main.js
@@ -1,7 +1,7 @@
 export const topMenuMain = (render, model) => render(
   model,
   ':topMenuMain'
-)`<nav class="Menu Menu--top Menu--center">
+)`<nav class="Menu Menu--top Menu--left">
 <ul class="Menu-list">
   <li id="button-show-navigation-menu"><button class="TextButton" is="nav-menu-toggle" id="nav-menu-toggle" data-menu-id='NavSidebar'>Menu</button></li>
 </ul>
