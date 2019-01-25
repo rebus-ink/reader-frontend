3ef8225d3957225403d2718d0ac644648bd83d86
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-01-25 16:06:29 -0500

    fix: remove unnecessary chapter wrapper

--- views/chapter.js
+++ views/chapter.js
@@ -2,7 +2,5 @@ import { clean } from '../server/utils/sanitize-state'
 
 export const chapterView = (render, model) => {
   const { chapter = {} } = model
-  return render(model, ':chapter')`<div class="Chapter-body">
-${[clean(chapter.content)]}
-</div>`
+  return render(model, ':chapter')`${[clean(chapter.content)]}`
 }
