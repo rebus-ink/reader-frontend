030e9271c63337fa25a778400ce43d1bc3ab5064
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-02-15 12:38:46 -0500

    feat: first pass at marker rich text area
    Not sure if this works.

--- components/annotations/marker.js
+++ components/annotations/marker.js
@@ -107,21 +107,19 @@ wickedElements.define('#sidebar', {
   }
 })
 
-wickedElements.define('[is="marker-input"]', {
+wickedElements.define('[is="marker-button"]', {
   onconnected (event) {
     this.el = event.currentTarget
-    this.textarea = event.currentTarget.querySelector('.Marker-textarea')
-    this.textarea.addEventListener('focus', this)
-    this.textarea.addEventListener('blur', this)
-    this.textarea.addEventListener('paste', this)
-    this.el.dataset.reader = true
+    this.el.addEventListener('click', this)
   },
-  onfocus (event) {
-    this.el.classList.add('Marker--focused')
-  },
-  onblur (event) {
-    this.el.classList.remove('Marker--focused')
-    this.textarea.textContent = this.textarea.textContent
+  onclick (event) {
+    const newMarker = marker(this.el.dataset.for)
+    const parent = this.el.parentElement
+    parent.removeChild(this.el)
+    parent.appendChild(newMarker)
+    window.requestAnimationFrame(function () {
+      newMarker.querySelector('.ql-editor').focus()
+    })
   }
 })
 
@@ -204,6 +202,53 @@ wickedElements.define('[is="reader-note"]', {
   }
 })
 
+wickedElements.define('[is="marker-note"]', {
+  init (event) {
+    const element = (this.el = event.currentTarget)
+    const container = this.el.querySelector('.MarkerNote-textarea')
+    this.quill = new window.Quill(container, {
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'underline'],
+          ['image', 'code-block'],
+          ['link', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['clean']
+        ]
+      },
+      theme: 'snow'
+    })
+    this.toolbar = this.el.querySelector('.ql-toolbar')
+    const onFocus = this.onfocus
+    const onBlur = this.onblur
+    this.quill.on('selection-change', function (range, oldRange, source) {
+      if (range) {
+        onFocus(element)
+      } else {
+        onBlur(element)
+      }
+    })
+    this.quill.on('text-change', function () {
+      element.parentElement.classList.add('Marker--hasContent')
+    })
+    const button = document.createElement('button')
+    button.textContent = 'Delete'
+    button.classList.add('TextButton')
+    button.classList.add('TextButton--noteDelete')
+    button.addEventListener('click', event => {
+      this.el.parentElement.removeChild(this.el)
+    })
+    button.dataset.noteDelete = 'true'
+    this.el.appendChild(button)
+  },
+  onfocus (element) {
+    element.classList.add('MarkerNote--hasFocus')
+  },
+  onblur (element) {
+    element.classList.remove('MarkerNote--hasFocus')
+  }
+})
+
 function note (element) {
   const xpath = element.dataset.xpath
   const form = document.createElement('form')
@@ -215,6 +260,27 @@ function note (element) {
   const textareaId = 'ReaderNote-text-' + xpath
   form.dataset.newNote = 'true'
   form.innerHTML = `<div class="ReaderNote-textarea" id="${textareaId}" data-reader="true" aria-label="Sidebar note"></div>`
+  const button = document.createElement('button')
+  button.textContent = 'Cancel'
+  button.classList.add('TextButton')
+  button.classList.add('TextButton--noteButton')
+  button.addEventListener('click', event => {
+    form.parentElement.removeChild(form)
+  })
+  button.dataset.noteCancel = 'true'
+  form.appendChild(button)
+  return form
+}
+function marker (xpath) {
+  const form = document.createElement('form')
+  form.setAttribute('is', 'marker-note')
+  form.dataset.for = xpath
+  form.dataset.reader = 'true'
+  form.classList.add('MarkerNote')
+  form.id = 'MarkerNote-' + xpath
+  const textareaId = 'MarkerNote-text-' + xpath
+  form.dataset.newNote = 'true'
+  form.innerHTML = `<div class="MarkerNote-textarea" id="${textareaId}" data-reader="true" aria-label="Sidebar note"></div>`
   const button = document.createElement('button')
   button.textContent = 'Cancel'
   button.classList.add('TextButton')
