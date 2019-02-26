942bec8e99dc44326ed9722767fcb9a0f329a122
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-02-26 15:21:37 -0500

    fix: library styles

--- app/components/library.js
+++ app/components/library.js
@@ -3,17 +3,18 @@ import {arrify} from '../utils/arrify.js'
 import {bookCard} from './book-card.js'
 
 export function library (context) {
+  const title = 'Library'
+  const bookCount = arrify(context.items).length
   return html`
   <main class="Library" id="Library">
-  <div class="LibraryMenu"><details class="MenuButton">
-  <summary class="MenuButton-summary">Library</summary>
-  <details-menu role="menu" class="MenuButton-body">
-    <ol>
-    <li><a href="/Library">Library</a></li>
-    <li><a href="/Import">Import</a></li>
-    </ol>
-  </details-menu>
-  </details></div>
+  <div class="Library-header">
+    <h1 class="Library-title">${title}</h1>
+    <nav class="Library-buttons">
+    <ol class="Library-buttons-list">
+    <li class="Library-buttons-item"><a href="/Import" class="MenuItem">Import</a></li>
+    </ol></nav>
+    <p class="Library-info">${bookCount} items in collection</p>
+  </div>
   <div class="Library-books">${arrify(context.items).map(book =>
     bookCard(book)
   )}</div>
