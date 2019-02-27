3f5a37a174061ba9245386e4013f16ac72781877
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-02-27 13:13:21 -0500

    fix: final move of rendering of library clientside

--- server/routes/library.js
+++ server/routes/library.js
@@ -5,19 +5,13 @@ const { ensureLogin } = require('../ensure-login.js')
 const { getUserStreams } = require('../utils/get-user-streams.js')
 const express = require('express')
 const router = express.Router()
-const { getLibraryState } = require('../utils/get-library-state.js')
 const debug = require('debug')('vonnegut:routes:library')
 
 router.get('/library', ensureLogin, getUserStreams, function (req, res, next) {
   debug(req.path)
-  return getLibraryState(req, res)
-    .then(model => {
-      debug('got model')
-      const render = viperHTML.wire
-      res.type('html')
-      res.send(page(render, model, req, pageBody))
-    })
-    .catch(err => next(err))
+  const render = viperHTML.wire
+  res.type('html')
+  res.send(page(render, {}, req, pageBody))
 })
 
 module.exports = router
