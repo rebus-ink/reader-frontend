cf0e836b7158e2d4702e4a95b2b5600c65bbd66f
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-03-03 18:41:55 -0500

    feat: book/toc menu

---
+++ app/views/chapter-book-menu.js
@@ -0,0 +1,35 @@
+
+import {html} from 'lighterhtml'
+import {arrify} from '../utils/arrify.js'
+
+// Need to make sure this has a return link and location markers
+export function bookMenu (state) {
+  const {book} = state
+  return html`<details class="MenuButton">
+  <summary class="MenuButton-summary">${book.name}</summary>
+  <ol class="MenuButton-body">
+  <li><a href="/library" class="MenuItem">Return to Library</a></li>
+  <li><hr class="MenuButton-separator"></li>
+  <li>
+    <ol>${renderToC(state)}</ol>
+  </li>
+  </ol>
+  </details>`
+}
+
+function renderToC (state) {
+  const {book, bookId, bookPath} = state
+  return arrify(book.orderedItems).map((chapter, index) => {
+    const url = `/reader/${bookId}/${
+      chapter['reader:path']
+    }`
+    const isSelected =
+      bookPath === chapter['reader:path']
+        ? 'is-selected'
+        : ''
+    const ariaCurrent = bookPath ? 'page' : false
+    return html`<li class="${isSelected}"><a href="${url}" class="MenuItem" aria-current=${ariaCurrent}>${
+      chapter.name
+    }</a></li>`
+  })
+}
--- app/views/chapter.js
+++ app/views/chapter.js
@@ -1,25 +1,13 @@
 import {html} from 'lighterhtml'
-import { tocSidebar } from './toc-sidebar.js'
-import { tocButton } from './toc-button.js'
+import { bookMenu } from './chapter-book-menu.js'
 import { floatingButtons } from './floating-buttons.js'
 
 export function chapter (state) {
-  const {dom, book} = state
-  return html`${tocSidebar(state)}
-  <nav class="Menu Menu--reader" id="NavMenu">
+  const {dom} = state
+  return html`<nav class="Menu Menu--reader" id="NavMenu">
 <ul class="Menu-list">
-  <li>${tocButton(state)}</li>
   <li>
-  <details class="MenuButton">
-<summary class="MenuButton-summary">${book.name}</summary>
-<ol class="MenuButton-body">
-<li><a href="/library" class="MenuItem">Return to Library</a></li>
-<li><hr class="MenuButton-separator"></li>
-<li><button disabled href="${window.location.pathname + '?notes=true'}" class="MenuItem">Notes view</button></li>
-<li><button disabled href="${window.location.pathname +
-  '?settings=true'}" class="MenuItem">Reading Settings</button></li>
-</ol>
-</details></li>
+  ${bookMenu(state)}</li>
 </ul>
 ${floatingButtons(state)}
 </nav>
