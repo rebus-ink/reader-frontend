915f47c39aaa10536742adc1baa4350804bc577e
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-03-11 15:51:19 -0400

    fix: better annotation affordances

--- app/annotations/reader-markers.js
+++ app/annotations/reader-markers.js
@@ -28,8 +28,12 @@ wickedElements.define('reader-markers', {
       this.state = activities.note(this.element.dataset.noteId)
       this.render()
     }
+    this.element.addEventListener('keyup', this)
+    this.checkHeight()
   },
   ondisconnected (event) {
+    this.element.removeEventListener('keyup', this)
+    this.element.removeEventListener('reader:markers-visible', this)
     // remove drop down marker element from sidebar
   },
   'onreader:markers-visible': function (event) {
@@ -38,7 +42,7 @@ wickedElements.define('reader-markers', {
       this.element.appendChild(tools)
     }
     const parent = this.element.closest('[data-location]')
-    const noteButton = html`<button class="NoteButton" data-component="note-button" aria-label="Add note" data-for="${parent.dataset.location}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="bevel"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>`
+    const noteButton = html`<button class="NoteButton" data-component="note-button" aria-label="Add note" data-for="${parent.dataset.location}"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="bevel"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>`
     parent.appendChild(noteButton)
     this.element.classList.add('Marker--visible')
   },
@@ -86,6 +90,21 @@ wickedElements.define('reader-markers', {
     } else {
     }
   },
+  onkeyup (event) {
+    this.checkHeight()
+  },
+  checkHeight () {
+    const textarea = this.element.querySelector('.Marker-textarea')
+    if (textarea) {
+      while (textarea.scrollHeight > textarea.clientHeight) {
+        textarea.style.overflow = 'hidden'
+        textarea.rows += 1
+      }
+      if (textarea.scrollHeight > textarea.clientHeight) {
+        textarea.style.overflow = 'auto'
+      }
+    }
+  },
   render () {
     if (this.state) {
       render(this.element, () => view(this.state))
@@ -98,5 +117,5 @@ wickedElements.define('reader-markers', {
 })
 
 function view (state) {
-  return html`<textarea  onchange="${this}" class="Marker-textarea" aria-label="Sidebar note" placeholder="...">${state.summary}</textarea><button class="Marker-button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="bevel"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg></button>`
+  return html`<textarea  onchange="${this}" class="Marker-textarea" aria-label="Sidebar note" placeholder="..." rows="1">${state.summary}</textarea><button class="Marker-button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="bevel"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg></button>`
 }
