308647666de36a6d690f0ad332bbe5a2daa3b952
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-04-17 15:03:57 -0400

    feat: library reducers 95% test coverage

---
+++ app/library/reducer.js
@@ -0,0 +1,186 @@
+
+import * as activities from '../state/activities.js'
+import {errorEvent} from '../utils/error-event.js'
+import {Book} from '../formats/Book.js'
+import {Article} from '../formats/Article.js'
+import {Epub} from '../formats/Epub/index.js'
+Book.activities(activities)
+
+const uploadingFiles = new Set()
+
+let zip
+/* istanbul ignore next */
+function zipModule () {
+  zip = import('./zip.js')
+  return zip
+}
+
+export function setState (state, status) {
+  return {...state, uploads: Array.from(uploadingFiles), status}
+}
+/* istanbul ignore next */
+export function reducer (state, action) {
+  let status = 'loaded'
+  switch (action.type) {
+    // This action is called by the main library component on every render.
+    case 'loading':
+      loading(state, action.dispatch, activities)
+      status = 'loaded'
+      break
+    case 'update':
+      state = action.state
+      status = 'updated'
+      break
+    case 'upload-queued':
+      status = 'upload-queued'
+      break
+    case 'file':
+      add(action, {activities, Epub, Article, zipModule})
+      status = 'file'
+      break
+    case 'files':
+      addFiles(action, {activities, Epub, Article, zipModule})
+      status = 'files'
+      break
+    case 'article':
+      add(action, {activities, Epub, Article, zipModule})
+      status = 'article'
+      break
+    case 'create-collection':
+      createCollection(action)
+      status = 'creating-collection'
+      break
+    case 'add-to-collection':
+      addToCollection(action)
+      status = 'adding-to-collection'
+      break
+    case 'remove-from-collection':
+      removeFromCollection(action)
+      status = 'removing-from-collection'
+      break
+    case 'activities-update':
+      activitiesUpdate(action, activities)
+      status = 'activities-update'
+      break
+    case 'error-event':
+      errorEvent(action.err)
+      status = 'error-event'
+      break
+    default:
+      return setState(state, status)
+  }
+  return setState(state, status)
+}
+
+export async function activitiesUpdate ({activity, payload, dispatch}, activities) {
+  try {
+    await activities[activity](payload)
+    dispatch({type: 'loading', dispatch})
+  } catch (err) {
+    if (err.response && err.response.status === 400) {
+      dispatch({type: 'loading', dispatch})
+    } else {
+      dispatch({type: 'error-event', err})
+    }
+  }
+}
+
+export function addFiles ({files, url, dispatch}, context) {
+  for (let file of files) {
+    add({file, url, dispatch}, context)
+  }
+}
+
+export function add ({file = {}, url, dispatch}, context) {
+  uploadingFiles.add({file, name: file.name, type: file.type, url, dispatch})
+  queue(context)
+}
+
+let pending
+
+function queue (context) {
+  const uploading = Array.from(uploadingFiles)
+  if (!pending && uploading[0]) {
+    pending = taskPromise(uploading[0], context)
+  }
+}
+
+async function taskPromise (action, context) { // testable
+  const {activities, Epub, Article, zipModule} = context
+  const {file, url, dispatch} = action
+  console.log('queueing new book')
+  let book
+  if (file && file.type === 'application/epub+zip') {
+    await zipModule()
+    const preview = new Epub()
+    const detail = { file, fileName: file.name }
+    book = await preview.initAsync({detail})
+  } else if (url) {
+    const preview = new Article()
+    book = await preview.initAsync(url)
+  }
+  dispatch({type: 'upload-queued'})
+  return book.uploadMedia()
+    .then(() => {
+      return activities.create(book.activity)
+    }).then(() => {
+      pending = null
+      uploadingFiles.delete(action)
+      dispatch({type: 'loading', dispatch})
+      queue(context)
+    })
+    .catch(err => {
+      err.httpMethod = 'Book Upload'
+      err.book = book
+      dispatch({type: 'error-event', err})
+    })
+}
+
+export async function loading (state, dispatch, activities) {
+  const newState = await activities.library()
+  if (newState.updated > state.updated) {
+    state.status = 'loaded'
+    dispatch({type: 'update', state: newState})
+  }
+}
+
+export function createCollection (action) { // testable
+  const {dispatch} = action
+  const payload = {
+    type: 'reader:Stack',
+    name: action.tag.name
+  }
+  dispatch({type: 'activities-update', dispatch, payload, activity: 'create'})
+}
+
+export function removeFromCollection (action) {
+  const {dispatch} = action
+  const payload = {
+    type: 'Remove',
+    object: {
+      type: 'reader:Stack',
+      id: action.tag.id,
+      name: action.tag.name
+    },
+    target: {
+      id: action.book
+    }
+  }
+  dispatch({type: 'activities-update', dispatch, payload, activity: 'remove'})
+}
+
+export function addToCollection (action) {
+  const {dispatch} = action
+  const payload = {
+    type: 'Add',
+    object: {
+      type: 'reader:Stack',
+      id: action.tag.id,
+      name: action.tag.name
+    },
+    target: {
+      id: action.book
+    }
+  }
+  dispatch({type: 'activities-update', dispatch, payload, activity: 'add'})
+}
---
+++ tests/components-library-reducers.js
@@ -0,0 +1,244 @@
+const tap = require('tap')
+const sinon = require('sinon')
+const reducers = require('../app/library/reducer.js')
+
+tap.test('setState', test => {
+  const state = reducers.setState({ test: 'test' }, 'what-what')
+  test.matches({ test: 'test', status: 'what-what', uploads: [] }, state)
+  test.end()
+})
+
+tap.test('loading', async test => {
+  const stateObj = { test: 'New State', updated: '2019-04-17T18:07:19.845Z' }
+  let testState
+  await reducers.loading(
+    { updated: '2019-04-17T18:07:05.632Z', test: 'Old State' },
+    ({ state }) => {
+      testState = state
+    },
+    { library: () => Promise.resolve(stateObj) }
+  )
+  test.matches(stateObj, testState)
+  test.end()
+})
+
+tap.test('create-collection', async test => {
+  const tag = { name: 'test' }
+  let testPayload
+  await reducers.createCollection({
+    tag,
+    dispatch: ({ payload }) => {
+      testPayload = payload
+    }
+  })
+  test.matches(
+    {
+      type: 'reader:Stack',
+      name: 'test'
+    },
+    testPayload
+  )
+  test.end()
+})
+
+tap.test('remove-from-collection', async test => {
+  const tag = { type: 'reader:Stack', name: 'test', id: 'id' }
+  const refPayload = {
+    type: 'Remove',
+    object: tag,
+    target: {
+      id: 'bookId'
+    }
+  }
+  let testPayload
+  await reducers.removeFromCollection({
+    tag,
+    book: 'bookId',
+    dispatch: ({ payload }) => {
+      testPayload = payload
+    }
+  })
+  test.matches(refPayload, testPayload)
+  test.end()
+})
+
+tap.test('add-to-collection', async test => {
+  const tag = { type: 'reader:Stack', name: 'test', id: 'id' }
+  const refPayload = {
+    type: 'Add',
+    object: tag,
+    target: {
+      id: 'bookId'
+    }
+  }
+  let testPayload
+  await reducers.addToCollection({
+    tag,
+    book: 'bookId',
+    dispatch: ({ payload }) => {
+      testPayload = payload
+    }
+  })
+  test.matches(refPayload, testPayload)
+  test.end()
+})
+
+tap.test('activities-update', async test => {
+  const tag = { type: 'reader:Stack', name: 'test', id: 'id' }
+  const activity = 'add'
+  const refPayload = {
+    type: 'Add',
+    object: tag,
+    target: {
+      id: 'bookId'
+    }
+  }
+  let testType, testPayload
+  await reducers.activitiesUpdate(
+    {
+      activity,
+      payload: refPayload,
+      dispatch: action => {
+        testType = action.type
+      }
+    },
+    {
+      add: payload => {
+        testPayload = payload
+        return Promise.resolve({ test: 'test' })
+      }
+    }
+  )
+  test.matches('loading', testType)
+  test.matches(refPayload, testPayload)
+  test.end()
+})
+
+tap.test('activities-update - 400 error', async test => {
+  const activity = 'add'
+  let testType
+  await reducers.activitiesUpdate(
+    {
+      activity,
+      payload: {},
+      dispatch: action => {
+        testType = action.type
+      }
+    },
+    {
+      add: payload => {
+        const err = new Error('test error')
+        err.response = { status: 400 }
+        throw err
+      }
+    }
+  )
+  test.matches('loading', testType)
+  test.end()
+})
+
+tap.test('activities-update - regular error', async test => {
+  const activity = 'add'
+  let testType
+  await reducers.activitiesUpdate(
+    {
+      activity,
+      payload: {},
+      dispatch: action => {
+        testType = action.type
+      }
+    },
+    {
+      add: payload => {
+        const err = new Error('test error')
+        throw err
+      }
+    }
+  )
+  test.matches('error-event', testType)
+  test.end()
+})
+
+class Epub {
+  initAsync (config) {
+    this._Epub = false
+    this._Article = false
+    if (config.detail) {
+      this._Epub = true
+    } else if (config) {
+      this._Article = true
+    }
+    return Promise.resolve(this)
+  }
+  uploadMedia () {
+    return Promise.resolve(this)
+  }
+  get activity () {
+    return { type: 'Activity', epub: this._Epub, article: this._Article }
+  }
+}
+
+tap.test('files - basic add', test => {
+  let testActivity
+  const context = {
+    Epub,
+    Article: Epub,
+    zipModule: () => Promise.resolve(),
+    activities: {
+      create (activity) {
+        testActivity = activity
+      }
+    }
+  }
+  let count = 0
+  const action = {
+    files: [
+      { name: 'file.epub', type: 'application/epub+zip' },
+      { name: 'file2.epub', type: 'application/epub+zip' }
+    ],
+    dispatch: act => {
+      test.ok(act.type)
+      if (act.type === 'loading') {
+        test.matches(testActivity, {
+          type: 'Activity',
+          epub: true,
+          article: false
+        })
+        count = ++count
+      }
+      if (count === 2) {
+        test.end()
+      }
+    }
+  }
+  reducers.addFiles(action, context)
+})
+
+tap.test('files - article add', test => {
+  let testActivity
+  const context = {
+    Epub,
+    Article: Epub,
+    zipModule: () => Promise.resolve(),
+    activities: {
+      create (activity) {
+        testActivity = activity
+      }
+    }
+  }
+  const action = {
+    url: 'https://example.com/',
+    dispatch: act => {
+      test.ok(act.type)
+      if (act.type === 'loading') {
+        test.matches(testActivity, {
+          type: 'Activity',
+          epub: false,
+          article: true
+        })
+        test.end()
+      }
+    }
+  }
+  reducers.add(action, context)
+})
