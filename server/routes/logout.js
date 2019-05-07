bbe0569050d0342ebb3f396467127e7bfa28d033
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-05-07 14:48:45 -0400

    fix: implement auth0 single log out

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -6,7 +6,11 @@ const csurf = require('csurf')
 router.post('/logout', ensureLogin, csurf(), (req, res) => {
   req.session = null
   req.logout()
-  res.redirect('/')
+  res.redirect(
+    `https://${process.env.AUTH0_DOMAIN}/v2/logout?client_id=${
+      process.env.AUTH0_CLIENT_ID
+    }&returnTo=${process.env.BASE}/`
+  )
 })
 
 module.exports = router
