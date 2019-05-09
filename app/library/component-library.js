752eee1b52beab09d1eb8fd3e92db11cdaae18a8
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-05-09 15:22:30 -0400

    fix: removed full screen feature

--- app/library/component-library.js
+++ app/library/component-library.js
@@ -34,7 +34,7 @@ export const Library = component((context, {dispatch}) => {
   <option value="${`${pathname}?order=alpha&desc=true`}" selected=${query.get('order') === 'alpha' && query.get('desc') === 'true'} aria-label="Alphabetical, descending">Z-A</option>
   </select></label>
     </div>
-    <div class="Library-books">${arrify(items).map(book => viewBook(book, arrify(state.tags), dispatch))}</div><button data-component="full-screen" class="Button Button--floating" aria-label="Full screen" id="full-screen-button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg></button></div>`
+    <div class="Library-books">${arrify(items).map(book => viewBook(book, arrify(state.tags), dispatch))}</div></div>`
   } else {
     return viewLoading()
   }
