10e625e27250829f25b51ef81f4907ab66904f60
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-05-27 14:58:07 -0400

    fix: mobile support on login page

--- server/views/login-page.js
+++ server/views/login-page.js
@@ -5,6 +5,8 @@ module.exports.page = (render, model, req, body) => {
 <head>
 <link media="all" rel="stylesheet" href="/static/styles/app.css">
 <title>Rebus Reader</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 ${body(render, model, req)}
 </html>`
