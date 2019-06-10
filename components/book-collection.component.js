66a3743289543988d48f55330e78584a00745b67
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-06-10 16:49:42 -0400

    feat: book-collection

---
+++ components/book-collection.component.js
@@ -0,0 +1,89 @@
+import { html } from 'lit-html'
+import { component } from 'haunted'
+import { repeat } from 'lit-html/directives/repeat'
+
+export const title = 'Book Collection Component: `<book-collection>`'
+
+export const description = `Given a list of books and a view config, renders that collection.`
+
+// http://localhost:8080/demo/?component=/components/book-collection.component.js&imports=/components/book-listing.component.js
+export const preview = () => {
+  const books = [
+    {
+      name: 'Book Title 1',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 2',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 3',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 4',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 5',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 6',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 7',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 8',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 9',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 10',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    }
+  ]
+  return html`<book-collection .books=${books}></book-collection>`
+}
+
+export const BookCollection = component(({ books = [] }) => {
+  console.log(books)
+  return html`
+    <style>
+.collection {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(6rem, 0.5fr));
+  grid-gap: 0.5rem;
+  padding-bottom: 1rem;
+}
+book-listing {
+  display: block;
+}</style>
+    <div class="collection">
+${repeat(
+    books,
+    item => item.id,
+    (item, index) =>
+      html`<book-listing .book=${item} index=${index}></book-listing>`
+  )}
+    </div>`
+}, window.HTMLElement)
+
+window.customElements.define('book-collection', BookCollection)
---
+++ components/recent-books.component.js
@@ -0,0 +1,124 @@
+import { html } from 'lit-html'
+import { component, useEffect, useContext, useState } from 'haunted'
+import { classMap } from 'lit-html/directives/class-map.js'
+import { ApiContext } from './api-provider.component.js'
+import { createAPI } from './api.state.js'
+
+export const title = 'Recent books: `<recent-books>`'
+
+export const description = `Ten most recent books for any given tag.`
+
+// http://localhost:8080/demo/?component=/components/recent-books.component.js&imports=/components/book-collection.component.js,/components/book-listing.component.js
+export const preview = () => {
+  const api = createAPI()
+  const books = [
+    {
+      name: 'Book Title 1',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 2',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 3',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 4',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 5',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 6',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 7',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 8',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 9',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    },
+    {
+      name: 'Book Title 10',
+      id: 'https://example.com/id',
+      attributedTo: [{ name: 'Fancy Author' }]
+    }
+  ]
+  api.library = params => {
+    return Promise.resolve({ items: books })
+  }
+  return html`<api-provider .value=${api}><recent-books></book-listing></api-provider>`
+}
+
+export const RecentBooks = component(
+  ({ tag = 'all' }) => {
+    const api = useContext(ApiContext)
+    const [books, setBooks] = useState([])
+    useEffect(() => {
+      let params
+      if (tag !== 'all') {
+        params = { stack: tag }
+      }
+      api.library(params).then(collection => setBooks(collection.items))
+      api.events.on('library-changed', () => {
+        return api
+          .library(params)
+          .then(collection => setBooks(collection.items))
+      })
+    }, [])
+    const url = `/library/collection/${tag}`
+    return html`<style>
+    .header-row {
+      display: flex;
+      justify-content: space-between;
+      align-content: center;
+      min-height: 30px;
+      margin-bottom: 0.5rem;
+    }
+    .label {
+      font-size: 0.8rem;
+      text-transform: uppercase;
+      color: var(--medium);
+      margin: 0;
+      margin-top: 0.25rem;
+    }
+    .link {
+      font-size: 0.8rem;
+      margin: 0;
+      margin-top: 0.25rem;
+    }
+    .link a {
+      text-decoration: none;
+    }
+  </style><div class=${classMap({
+    'header-row': true
+  })}><p class="label">Recent files ${
+  tag === 'all' ? '' : `in ${tag}`
+}</p> <p class="link">
+    <a href=${url}>See all &gt;</a>
+  </p></div><book-collection .books=${books}></book-collection>`
+  },
+  window.HTMLElement,
+  { useShadowDOM: false }
+)
+
+window.customElements.define('recent-books', RecentBooks)
