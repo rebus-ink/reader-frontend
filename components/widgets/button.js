b58aa017cf8a27831f2b3a0ff7895f83429a45ca
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-06-11 00:44:45 -0400

    feat: fetch buttons

--- components/widgets/button.js
+++ components/widgets/button.js
@@ -7,10 +7,16 @@ export const title = 'Button: `<ink-button>`'
 export const description = `The default button`
 // http://localhost:8080/demo/?component=/components/widgets/button.js
 export const preview = () => {
-  return html`<ink-button>Fancy Button</ink-button> <ink-button secondary>Secondary Button</ink-button><ink-button disabled>Disabled Button</ink-button><ink-button dropdown>Dropdown Button</ink-button><ink-button dropdown secondary>Dropdown Button</ink-button>`
+  return html`<ink-button>Fancy Button</ink-button> <ink-button secondary>Secondary Button</ink-button><ink-button disabled>Disabled Button</ink-button><ink-button dropdown>Dropdown Button</ink-button><ink-button dropdown secondary>Dropdown Button</ink-button><ink-button dropdown compact secondary>Dropdown Button</ink-button><ink-button dropdown compact>Dropdown Button</ink-button><ink-button working>Fetching</ink-button><ink-button working secondary>Fetching</ink-button>`
 }
 
-export const InkButton = ({ disabled, secondary, dropdown, compact }) => {
+export const InkButton = ({
+  disabled,
+  secondary,
+  dropdown,
+  compact,
+  working
+}) => {
   return html`<style>
     :host(:not([hidden])) {
       display: inline-block;
@@ -43,6 +49,7 @@ button {
   border: 2px solid var(--rc-main);
   box-shadow: 1px 2px 4px 0 rgba(33, 33, 33, 0.1);
   text-decoration: none !important;
+  position: relative;
 }
 .dropdown {
   padding-right: 1.5rem;
@@ -115,15 +122,50 @@ button[disabled]:focus, {
   padding-top: 0.25rem;
   padding-bottom: 0.25rem;
 }
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.working {
+  padding-right: 1.75rem;
+}
+.working svg {
+  display: inline-block;
+  content: '';
+    position: absolute;
+    top: 7px;
+    right: 10px;
+  animation: spin 0.5s infinite;
+  width: 16px;
+  height: 16px;
+}
   </style>
       <button ?disabled=${disabled} class=${classMap({
   secondary,
   dropdown,
-  compact
-})}><slot>Button</slot></button>`
+  compact,
+  working
+})}><slot>Button</slot>${
+  working
+    ? html`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg>`
+    : ''
+}</button>`
 }
 
-InkButton.observedAttributes = ['disabled', 'secondary', 'dropdown', 'compact']
+InkButton.observedAttributes = [
+  'disabled',
+  'secondary',
+  'dropdown',
+  'compact',
+  'working'
+]
 
 window.customElements.define(
   'ink-button',
